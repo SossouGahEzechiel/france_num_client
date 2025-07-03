d3d007619d4f07ffc2842c7dec5b507d16a4042b
SossouGahEzechiel <ezecsossougah@gmail.com>
2025-07-03 17:59:57 +0000

    Update use-api.js

--- composables/use-api.js
+++ composables/use-api.js
@@ -1,6 +1,7 @@
 export const useApi = () => {
 	const config = useRuntimeConfig()
 
+	// ""
 	const apiBase = config.public.apiBase
 	// const apiBase = "http://localhost:3001/api"
 	console.log("ApiBase-c:", config.public);
